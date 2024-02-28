f67b910259a36f0d9491efa90cf3a6ccb71a9455
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-02-28 18:19:45 +0200

    Update src/js/2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import iconCheck from '../img/check-circle.svg';
-import iconError from '../img/error-octagon.svg';
+import iconError from '../img/x-octagon.svg';
 
 const form = document.querySelector(".form");
 const inputState = form.state;
